a4644ff5dfc3f17a25e7498570079bc95ad43d1a
herbeton <herbeton7@gmail.com>
2022-08-13 15:47:13 -0300

    Changes.

---
+++ src/database/dataSource.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm"
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "admin",
+    database: "cod_drops_crud",
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from "express";
+import "./database/dataSource";
 
 const app = express();
 
