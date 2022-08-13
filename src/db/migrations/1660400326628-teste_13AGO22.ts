7f065ea2faa28515b2cb900fc9384b1c65dea5e1
herbeton <herbeton7@gmail.com>
2022-08-13 15:47:43 -0300

    Changes.

---
+++ src/db/migrations/1660400326628-teste_13AGO22.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class teste13AGO221660400326628 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/db/migrations/1660416212850-CreateCategories.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class CreateCategories1660416212850 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
